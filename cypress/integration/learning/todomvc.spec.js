7a038efdc8542ce2a0f01e89253a55231ea90625
GABOCALVO\Gabriel <gabriel.calvo.vargas@gmail.com>
2020-04-16 17:00:37 -0600

    lesson 4 done, basic validation

--- cypress/integration/learning/todomvc.spec.js
+++ cypress/integration/learning/todomvc.spec.js
@@ -8,10 +8,18 @@ it('should be able to add a new todo to the list', ()=>{
 
     cy.visit('http://todomvc-app-for-testing.surge.sh')
 
-    cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}");
+    cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
 
-    cy.get('.toggle').click();
+    cy.get('label').should('have.text', 'Clean room')
 
-    cy.contains('Clear completed').click();
+    cy.get('.toggle').should('not.be.checked')
+
+    cy.get('.toggle').click()
+
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+
+    cy.contains('Clear').click()
+
+    cy.get('.todo-list').should('not.have.descendants', 'li')
 
 })
